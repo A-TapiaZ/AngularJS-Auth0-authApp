563960539f708e59f59fdbaef7078a893a5aa6d0
Alejo Tapias <alejotapias0414@gmail.com>
2020-10-01 13:57:32 -0500

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PreciosComponent } from './components/precios/precios.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
 
-const routes: Routes = [];
+// EL FAMOSO MIDDELWARE EN REACT, es un servicio para verificar que estoy autenticado dentro de la app
+import { AuthGuard } from './services/auth.guard';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'precios', component: PreciosComponent },
+  { path: 'protegida', component: ProtegidaComponent, canActivate:[AuthGuard]},
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
